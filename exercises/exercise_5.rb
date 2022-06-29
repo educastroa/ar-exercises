1a565f771f868b20dc31fb016dd26ebe34f0bba1
educastroa <eduardocastro.a@hotmail.com>
2022-06-29 19:00:09 +0000

    complete exercise four

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,26 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store2.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store1.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store2.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store1.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store2.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store1.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store2.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store1.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store2.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store1.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store2.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store1.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store2.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store1.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store2.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store1.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store2.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store1.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store2.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store1.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+@store2.employees.create(first_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", last_name: "#{(0...8).map { (65 + rand(26)).chr }.join.downcase}", hourly_rate: "#{rand(40..60)}" )
+
